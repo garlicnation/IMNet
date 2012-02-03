1ea1da11ffd6116593288c56ad9d2b508cf6961d
AJ Ortega <ajo@gsf.org>
2012-02-02 18:49:59 -0800

    Admin interfaces. Also modified models slightly.

---
+++ imnet/admin.py
@@ -0,0 +1,12 @@
+from imnet.models import *
+from django.contrib import admin
+
+admin.site.register(Artist)
+admin.site.register(Label)
+admin.site.register(Venue)
+admin.site.register(Event)
+admin.site.register(Album)
+admin.site.register(Track)
+admin.site.register(Image)
+admin.site.register(Message)
+admin.site.register(Post)
