0da01a0d096982c2b5847378e5704ce488feacb6
AJ Ortega <ajo@gsf.org>
2012-02-02 18:33:02 -0800

    Removed our useless User class. Now we use django.contrib.auth.User

--- imnet/models.py
+++ imnet/models.py
@@ -1,11 +1,5 @@
 from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=128)
-
-    def __unicode__(self):
-        return self.name
+from django.contrib.auth.models import User
 
 
 class MusicEntity(models.Model):
