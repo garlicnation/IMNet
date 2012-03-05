69a70aef2cb27edbca47442bb25d6b94522bcbfd
AJ Ortega <ajo@gsf.org>
2012-03-04 16:12:19 -0800

    AUTOCOMPLETE MESSAGE BITCHES. SUCK MY FAT WHITE #@$%

--- urls.py
+++ urls.py
@@ -19,7 +19,9 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^messages/', include('messages.urls'))
+    url(r'^messages/', include('messages.urls')),
+    url(r'^lookup/user/(?P<username>\w+)/$', 'imnet.views.user_lookup'),
+
 )
 
 urlpatterns += staticfiles_urlpatterns()
